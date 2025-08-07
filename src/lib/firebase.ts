326422030c63402a997f907bc4c98d77a645b599
Deepak Gupta <dg3667610@gmail.com>
2025-08-07 11:11:26 +0000

    const firebaseConfig = {   apiKey: "AIzaSyDCN-JN5O2ZpsA30B4sTgiS9Z4JPGnx

---
+++ src/lib/firebase.ts
@@ -0,0 +1,27 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDCN-JN5O2ZpsA30B4sTgiS9Z4JPGnx9f0",
+  authDomain: "my-portfolio-5qdx9.firebaseapp.com",
+  projectId: "my-portfolio-5qdx9",
+  storageBucket: "my-portfolio-5qdx9.firebasestorage.app",
+  messagingSenderId: "147079995859",
+  appId: "1:147079995859:web:100f22e213f95a2f1b0f26"
+};
+
+
+// Initialize Firebase
+let app;
+if (!getApps().length) {
+    app = initializeApp(firebaseConfig);
+} else {
+    app = getApps()[0];
+}
+
+const db = getFirestore(app);
+
+export { app, db };
