74663e64bf3cade4d32a189983c6660d782f0c4d
Deepak Gupta <dg3667610@gmail.com>
2025-09-02 09:24:27 +0000

    Why my firebase hosted website show
    Error: Server Error
    The server enco

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDCN-JN5O2ZpsA30B4sTgiS9Z4JPGnx9f0",
-  authDomain: "my-portfolio-5qdx9.firebaseapp.com",
-  projectId: "my-portfolio-5qdx9",
-  storageBucket: "my-portfolio-5qdx9.firebasestorage.app",
-  messagingSenderId: "147079995859",
-  appId: "1:147079995859:web:100f22e213f95a2f1b0f26"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 
